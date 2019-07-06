a353b2503dbd3f5082dfd3e44c666d957e6daf3a
Soham Zemse <sohamzemse143@gmail.com>
2019-07-06 17:21:25 +0530

    write compile script and run it

---
+++ compile.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const sourceFolderPath = path.resolve(__dirname, 'contracts');
+const buildFolderPath = path.resolve(__dirname, 'build');
+
+const getContractSource = contractFileName => {
+  const contractPath = path.resolve(__dirname, 'contracts', contractFileName);
+  const source = fs.readFileSync(contractPath, 'utf8');
+  return source;
+};
+
+let sources = {};
+
+fs.readdirSync(sourceFolderPath).forEach(contractFileName => {
+  sources = {
+    ...sources,
+    [contractFileName]: {
+      content: getContractSource(contractFileName)
+    }
+  }
+});
+//console.log(sources);
+
+const input = {
+    language: 'Solidity',
+    sources,
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+}
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+//console.log(output.contracts['Eraswap.sol']);
+
+
+if (Object.entries(output).length === 0) {
+  throw '\nError in compilation please check the contract\n';
+} else {
+  fs.removeSync(buildFolderPath);
+  fs.ensureDirSync(buildFolderPath);
+
+  for (let contractFile in output.contracts) {
+    let i = 0;
+    for(let key in output.contracts[contractFile]) {
+      console.log(key, Object.keys(output.contracts[contractFile][key]));
+      fs.outputJsonSync(
+        path.resolve(buildFolderPath, `${key}_${i}.json`),
+        output.contracts[contractFile][key]
+      );
+      i++;
+    }
+  }
+}
